23fd8cbf235d17b183786c422722b376d7f0c074
pgilmore <pagilmor@microsoft.com>
2019-03-05 12:47:44 -0800

    Deleting the 4.18 archive

--- 4.18/ExampleProject/Source/ExampleProject/ExampleProject.cpp
+++
@@ -1,7 +0,0 @@
-//////////////////////////////////////////////////////
-// Copyright (C) Microsoft. 2018. All rights reserved.
-//////////////////////////////////////////////////////
-
-#include "ExampleProject.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ExampleProject, "ExampleProject" );

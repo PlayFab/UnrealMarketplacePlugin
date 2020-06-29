43028357b4f60bbfd1ef3458a02f9f243ab8f2ab
Todd Bello <toddbell@microsoft.com>
2020-06-29 14:14:15 -0700

    Remove 4.22 from support
    We will only support the last 3 versions of Unreal: 4.25, 4.24 and 4.23

--- 4.22/ExampleProject/BuildAndRunTestsWin64.bat
+++
@@ -1,8 +0,0 @@
-rem # Running the build
-call "%~dp0BuildWin64.bat"
-
-rem # Running the Tests
-call "%~dp0RunTestsWin64.bat"
-
-cd "Build\Win64\WindowsNoEditor\ExampleProject\Saved\Logs"
-type ExampleProject.log

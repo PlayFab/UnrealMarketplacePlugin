e89871bf64e9d6ae9dbe7eaf7ee8d380dd50de12
Todd Bello <toddbell@microsoft.com>
2020-07-01 14:07:56 -0700

    Remove 4.22 from support (#28)

--- 4.22/ExampleProject/RunTestsWin64.bat
+++
@@ -1,6 +0,0 @@
-echo ======= RUNNING TESTS WIN64 =======
-
-rem # Destination Path, where the build will end up (relative to active directory).
-set archivePath=Build\Win64
-
-call "%~dp0%archivePath%\WindowsNoEditor\ExampleProject.exe" -run=RunTests

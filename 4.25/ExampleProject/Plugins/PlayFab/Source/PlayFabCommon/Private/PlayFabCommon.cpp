522efb62b83f6b9f4333f5f19e0b7fc49df161b2
Paul Gilmore <pagilmor@microsoft.com>
2020-12-02 00:20:23 -0800

    Restore the Project Setting registration

--- 4.25/ExampleProject/Plugins/PlayFab/Source/PlayFabCommon/Private/PlayFabCommon.cpp
+++ 4.25/ExampleProject/Plugins/PlayFab/Source/PlayFabCommon/Private/PlayFabCommon.cpp
@@ -19,6 +19,14 @@ DEFINE_LOG_CATEGORY(LogPlayFabCommon);
 
 class FPlayFabCommonModule : public IPlayFabCommonModuleInterface
 {
+    /** IModuleInterface implementation */
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+
+    // Settings
+    void RegisterSettings();
+    void UnregisterSettings();
+
     FString GetSdkVersion() const override { return PlayFabCommon::PlayFabCommonSettings::sdkVersion; }
     FString GetBuildIdentifier() const override { return PlayFabCommon::PlayFabCommonSettings::buildIdentifier; }
     FString GetVersionString() const override { return PlayFabCommon::PlayFabCommonSettings::versionString; }
@@ -49,6 +57,43 @@ class FPlayFabCommonModule : public IPlayFabCommonModuleInterface
 
     FString GeneratePfUrl(const FString& urlPath) const override { return PlayFabCommon::PlayFabCommonSettings::GeneratePfUrl(urlPath); }
 };
+
+void FPlayFabCommonModule::StartupModule()
+{
+    // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+    RegisterSettings();
+}
+
+void FPlayFabCommonModule::ShutdownModule()
+{
+    // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+    // we call this function before unloading the module.
+    UnregisterSettings();
+}
+
+void FPlayFabCommonModule::RegisterSettings()
+{
+#if WITH_EDITOR
+    if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+    {
+        ISettingsSectionPtr SettingsSection = SettingsModule->RegisterSettings("Project", "Plugins", "PlayFab",
+            LOCTEXT("PlayFabSettingsName", "PlayFab"),
+            LOCTEXT("PlayFabSettingsDescription", "Configure the PlayFab plugin"),
+            GetMutableDefault<UPlayFabRuntimeSettings>()
+        );
+    }
+#endif // WITH_EDITOR
+}
+
+void FPlayFabCommonModule::UnregisterSettings()
+{
+#if WITH_EDITOR
+    if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+    {
+        SettingsModule->UnregisterSettings("Project", "Plugins", "PlayFab");
+    }
+#endif
+}
 
 #undef LOCTEXT_NAMESPACE
 

c3feac346b95a72d14d44240119238728edcccd8
Todd Bello <toddbell@microsoft.com>
2021-04-13 15:40:29 -0700

    adding comment why we are adding a special rule

--- template/makeBp.js
+++ template/makeBp.js
@@ -72,7 +72,7 @@ function getDataTypeSafeName(apiElement, attrName) {
     var pfTypeName = apiElement[attrName];
     if (pfTypeName === "SourceType") // In Unreal, the Enum ESourceType exists in the Android builder and conflicts with our ESourceType enum
         return "PfSourceType";
-    if (pfTypeName === "TriggerType")
+    if (pfTypeName === "TriggerType") // In Unreal, the Enum ETriggerType exists in the Enhanced Input plugin and conflicts with our ETriggerType enum
         return "PfTriggerType"
     return pfTypeName;
 }

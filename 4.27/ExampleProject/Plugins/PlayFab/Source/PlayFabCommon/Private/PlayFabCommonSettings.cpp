3544d81bf94cdc46496037ccec3ab9df1e8feac0
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2022-08-16 18:34:18 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#220816

--- 4.27/ExampleProject/Plugins/PlayFab/Source/PlayFabCommon/Private/PlayFabCommonSettings.cpp
+++ 4.27/ExampleProject/Plugins/PlayFab/Source/PlayFabCommon/Private/PlayFabCommonSettings.cpp
@@ -7,9 +7,9 @@
 
 namespace PlayFabCommon
 {
-    const FString PlayFabCommonSettings::sdkVersion = "1.95.220815";
+    const FString PlayFabCommonSettings::sdkVersion = "1.96.220816";
     const FString PlayFabCommonSettings::buildIdentifier = "adobuild_unrealmarketplaceplugin_142";
-    const FString PlayFabCommonSettings::versionString = "UE4MKPL-1.95.220815";
+    const FString PlayFabCommonSettings::versionString = "UE4MKPL-1.96.220816";
 
     FString PlayFabCommonSettings::clientSessionTicket;
     FString PlayFabCommonSettings::entityToken;
